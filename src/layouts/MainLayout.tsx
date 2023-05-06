ede437841a17c1e535cdf299c51c141b39a1b084
Sepideh Khadempir <49390356+sepiddd@users.noreply.github.com>
2023-05-06 22:13:42 +0100

    5#add design system (#6)
    * chore: add antd to the project
    
    * chore: update gitignore file
    
    * chore: add main layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,61 @@
+import React, { ReactNode, useState } from 'react';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+
+const { Header, Sider, Content } = Layout;
+
+interface Props {
+  children: ReactNode;
+}
+
+const MainLayout: React.FC<Props> = ({ children }: Props) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{ minHeight: '100%' }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              label: 'My boards',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
